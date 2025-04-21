f6f976fd93258e8dc1885bd8bdf0ff398a0b3292
IannisG10 <guerraiannis@gmail.com>
2025-04-21 17:25:03 +0200

    define function pt_milieu

--- Point_Milieu.py
+++ Point_Milieu.py
@@ -3,3 +3,12 @@ def f(t,y):
 
 def define_node(a,i,h):
     return a + i*h
+
+
+def pt_milieu(func,h,ti,Wi):
+    k1 = ti + (h/2)
+    k2 = Wi + (h/2)*func(ti,Wi)
+    k3 = func(k1,k2)
+
+    return k3
+
