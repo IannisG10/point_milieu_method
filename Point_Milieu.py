fab26da54f27774cbba7d6d3ead4e261e0c2cc02
IannisG10 <guerraiannis@gmail.com>
2025-04-21 17:11:31 +0200

    All methods euler

--- Point_Milieu.py
+++ Point_Milieu.py
@@ -1,5 +1,14 @@
-def f():
-    return
+def f(t,y):
+    return -y + t + 1
 
 def define_node(a,i,h):
     return a + i*h
+
+def Taylor(f,h,t,y):
+    return h*f(t+(h/2),y+(h/2)*f(t,y))
+
+a = int(input("a"))
+b = int(input("b"))
+N = int(input("Nombre d'intersection"))
+alpha = int(input("alpha:"))
+step = (b-a)/N
