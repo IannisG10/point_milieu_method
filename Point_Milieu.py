97d7e9f865cf0bf89ae8d651351d3035ec1f8f78
IannisG10 <guerraiannis@gmail.com>
2025-04-21 17:17:14 +0200

    declare function  and node function define_node

--- Point_Milieu.py
+++ Point_Milieu.py
@@ -3,12 +3,3 @@ def f(t,y):
 
 def define_node(a,i,h):
     return a + i*h
-
-def Taylor(f,h,t,y):
-    return h*f(t+(h/2),y+(h/2)*f(t,y))
-
-a = int(input("a"))
-b = int(input("b"))
-N = int(input("Nombre d'intersection"))
-alpha = int(input("alpha:"))
-step = (b-a)/N
